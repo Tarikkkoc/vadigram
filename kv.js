c5bbedc48b4fcbefcaf86cb9195d8a08196bad9a
Tarikkoc <ahmettarikkoc@gmail.com>
2022-11-27 15:04:14 +0300

    second commit

---
+++ kv.js
@@ -0,0 +1,11 @@
+function home(){
+    location.href="firstPage.html";   
+}
+
+function search(){
+    location.href="secondPage.html";   
+}
+
+const profile = ()=>{
+    location.href = "thirdPage.html"
+}
